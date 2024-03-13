39961b01e6639c310ad7f3d7a39afd51ee6a2168
joey00072 <00shxf@gmail.com>
2024-03-13 10:49:40 +0000

    :bookmark: Ohara is not module now

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ohara",
+    version="0.1",
+    packages=find_packages(),
+    author="joey00072",
+    author_email="00shxf@gmail.com",
+    description="A collection of implementations of LLM, papers, and other models.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/joey00072/ohara",
+    install_requires=[
+        # Add your project dependencies here
+        # TODO: add requirements here 
+    ],
+    entry_points={
+        'console_scripts': [
+            'ohara=ohara.main:main',  # Adjust 'ohara.main:main' as necessary
+        ],
+    },
+    python_requires='>=3.6',
+)
