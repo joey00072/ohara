fc0c112cf01a0d4f3f5667e97ab574f9dabdb6ac
joey00072 <00shxf@gmail.com>
2024-07-23 07:06:35 +0530

    🎨 Added Qsparse traning script

--- experiments/q_sparse/q_sparse.py
+++ experiments/q_sparse/q_sparse.py
@@ -2,50 +2,55 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class QSparse(torch.autograd.Function):
     @staticmethod
     def forward(ctx, tensor, K):
         topk_values, topk_indices = torch.topk(tensor, K, dim=-1)
-        
+
         masked_tensor = torch.zeros_like(tensor)
         masked_tensor.scatter_(-1, topk_indices, topk_values)
-        
-        norm = torch.norm(masked_tensor, dim=-1, keepdim=True)
-        output = masked_tensor / norm
-        
-        ctx.save_for_backward(norm)
+
+        output = masked_tensor
+
         return output
-    
+
     @staticmethod
     def backward(ctx, grad_output):
-        norm, = ctx.saved_tensors
-        return grad_output/norm, None
-    
-    
+        return grad_output, None
+
+
 def q_sparse(tensor, K):
     return QSparse.apply(tensor, K)
 
+
 class QSparseLinear(nn.Linear):
-    def __init__(self,K,*args, **kwargs):
+    def __init__(self, pct, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.K = K
-    def forward(self, x):
+        self.K = int(pct * self.weight.shape[1])
+        print(self.in_features, self.out_features, self.K)
+
+    def forward(self, x: torch.Tensor):
         x = QSparse.apply(x, self.K)
+        x = x / torch.norm(x, dim=-1, keepdim=True)
         return F.linear(x, self.weight, self.bias)
-    
-    
-def monkey_patch_model(model: nn.Module,K:int, target_layers:list[str]):
+
+
+def monkey_patch_model(model: nn.Module, target_layers: list[str], pct: float = 0.7):
     for name, module in model.named_children():
         if isinstance(module, nn.Linear) and name in target_layers:
             setattr(
                 model,
                 name,
-                QSparseLinear(K, module.in_features, module.out_features, bias=module.bias is not None),
+                QSparseLinear(
+                    pct, module.in_features, module.out_features, bias=module.bias is not None
+                ),
             )
         else:
-            monkey_patch_model(module, K,target_layers)
+            monkey_patch_model(module, target_layers, pct)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # Example usage
     tensor = torch.randn(4, 10, requires_grad=True)
     K = 3
---
+++ ohara/modules/activations.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn.functional as F
+
+
+def relu_squared(x):
+    return torch.relu(x)**2
+
+ACT2FN = {
+    "relu": F.relu,
+    "silu": F.silu,
+    "gelu": F.gelu,
+    "tanh": torch.tanh,
+    "sigmoid": torch.sigmoid,
+    "relu_squared": relu_squared,
+    "relu2": relu_squared,
+}
