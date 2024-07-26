884013615287b4e9575ed65dfdeacd003b20f9c3
joey00072 <00shxf@gmail.com>
2024-07-26 12:27:45 +0530

    relu next

---
+++ experiments/relu_next/model_relu_next.py
@@ -0,0 +1,320 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+from dataclasses import dataclass
+from ohara.modules.activations import ACT2FN
+from ohara.modules.norm import RMSNorm
+
+from ohara.embedings_pos.rotatry import precompute_freqs_cis
+from ohara.embedings_pos.rotatry import apply_rope
+
+from typing import Callable
+
+from huggingface_hub import PyTorchModelHubMixin
+
+from collections import OrderedDict
+
+
+@dataclass
+class Config(OrderedDict):
+    vocab_size: int
+    seq_len: int
+
+    d_model: int
+    hidden_dim: int
+
+    num_heads: int
+    num_kv_heads: int = 0
+
+    num_layers: int = 4
+
+    dropout: float = 0.2
+    bias: bool = False
+    weight_tying: bool = False
+
+    activation: Callable = "silu"  # "relu", "gelu", "silu" etc
+    mlp: str = "GLU"  # MLP or GLU
+
+
+class ReLU2Function(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        return torch.relu(input).square()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (input,) = ctx.saved_tensors
+        r_input = torch.relu(input)
+        grad_input = ((input > 0) * r_input + input) * grad_output
+        return grad_input
+
+
+relu3 = ReLU2Function.apply
+ACT2FN["relu3"] = relu3
+
+
+def silulu(x):
+    return F.silu(x) + F.relu(x).square()
+
+
+ACT2FN["silulu"] = silulu
+
+
+class MLP(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        hidden_dim: int | None = None,
+        multiple_of: int = 4,
+        dropout: float | None = None,
+        activation_fn: str = "silu",
+        bias: bool = True,
+    ):
+        super().__init__()
+
+        if hidden_dim is None:
+            hidden_dim = 4 * dim
+            hidden_dim = int(2 * hidden_dim / 3)
+            hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
+
+        self.up = nn.Linear(dim, hidden_dim, bias=bias)
+        self.down = nn.Linear(hidden_dim, dim, bias=bias)
+        self.activation_fn = ACT2FN[activation_fn]
+
+        self.dropout = nn.Dropout(dropout) if dropout else lambda x: x
+
+    def forward(self, x):
+        x = self.up(x)
+        x = self.activation_fn(x)
+        x = self.down(x)
+        x = self.dropout(x)
+        return x
+
+
+class GLU(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        hidden_dim: int | None = None,
+        multiple_of: int = 4,
+        dropout: float | None = None,
+        activation: str = "silu",
+        bias: bool = False,
+    ):
+        """
+        GLU Variants Improve Transformer
+        https://arxiv.org/abs/2002.05202v1
+
+        order in which W1,W2,W3 are multiplied is as per llama (for compatiblity)
+        """
+        super().__init__()
+
+        if hidden_dim is None:
+            hidden_dim = 4 * dim
+            hidden_dim = int(2 * hidden_dim / 3)
+            hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
+
+        self.up = nn.Linear(dim, hidden_dim, bias=bias)
+        self.gate = nn.Linear(dim, hidden_dim, bias=bias)
+        self.down = nn.Linear(hidden_dim, dim, bias=bias)
+
+        self.activation = ACT2FN[activation]
+        self.dropout = nn.Dropout(dropout) if dropout else lambda x: x
+
+    def forward(self, x):
+        up = self.up(x)
+        gate = self.gate(x)
+        down = self.down(F.silu(gate) * up)
+        return self.dropout(down)
+
+
+MLP_BLOCK = {"MLP": MLP, "GLU": GLU}
+
+
+class Attention(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+
+        d_model = config.d_model
+        self.num_heads = config.num_heads
+        self.head_dim = config.d_model // config.num_heads
+        self.num_kv_heads = config.num_heads if config.num_kv_heads == 0 else config.num_kv_heads
+        assert self.num_heads % self.num_kv_heads == 0
+        self.num_queries_per_kv = self.num_heads // self.num_kv_heads
+
+        self.key = nn.Linear(d_model, self.head_dim * self.num_heads, config.bias)
+        self.query = nn.Linear(d_model, self.head_dim * self.num_kv_heads, config.bias)
+        self.value = nn.Linear(d_model, self.head_dim * self.num_kv_heads, config.bias)
+        self.proj = nn.Linear(d_model, d_model, config.bias)
+
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.res_dropout = nn.Dropout(config.dropout)
+
+        self.flash_attn = hasattr(torch.nn.functional, "scaled_dot_product_attention")
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor, freqs_cis) -> torch.Tensor:
+        batch, seq_len, d_model = x.shape
+
+        k: torch.Tensor  # type hint for lsp
+        q: torch.Tensor  # ignore
+        v: torch.Tensor
+
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+
+        k = k.view(
+            batch, seq_len, self.num_heads, self.head_dim
+        )  # shape = (B, seq_len, num_heads, head_dim)
+        q = q.view(batch, seq_len, self.num_heads, self.head_dim)
+        v = v.view(batch, seq_len, self.num_heads, self.head_dim)
+
+        q, k = apply_rope(q, k, freqs_cis)
+
+        # Grouped Query Attention
+        if self.num_kv_heads != self.num_heads:
+            k = torch.repeat_interleave(k, self.num_queries_per_kv, dim=2)
+            v = torch.repeat_interleave(v, self.num_queries_per_kv, dim=2)
+
+        k = k.transpose(1, 2)  # shape = (B, num_heads, seq_len, head_dim)
+        q = q.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        if self.flash_attn:
+            output = torch.nn.functional.scaled_dot_product_attention(
+                q,
+                k,
+                v,  # order impotent
+                attn_mask=None,
+                dropout_p=self.attn_dropout.p if self.training else 0.0,
+                is_causal=True,
+            )
+        else:
+            attn_mtx = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(self.head_dim)
+            attn_mtx = attn_mtx + mask[:, :, :seq_len, :seq_len]
+            attn_mtx = F.softmax(attn_mtx.float(), dim=-1).type_as(k)
+            attn_mtx = self.attn_dropout(attn_mtx)
+
+            output = torch.matmul(attn_mtx, v)  # (batch, n_head, seq_len, head_dim)
+
+        # restore time as batch dimension and concat heads
+        output = output.transpose(1, 2).contiguous().view(batch, seq_len, d_model)
+
+        # final projection into the residual stream
+        output = self.proj(output)
+        output = self.res_dropout(output)
+        return output
+
+
+class Block(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+
+        self.attn = Attention(config)
+        self.ff = MLP_BLOCK[config.mlp](
+            dim=config.d_model,
+            hidden_dim=config.hidden_dim,
+            dropout=config.dropout,
+            bias=config.bias,
+        )
+
+        self.norm1 = RMSNorm(config.d_model)
+        self.norm2 = RMSNorm(config.d_model)
+
+    def forward(self, x, mask, freqs_cis):
+        x = x + self.attn(self.norm1(x), mask, freqs_cis)
+        x = x + self.ff(self.norm2(x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, config: Config, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.config = config
+
+        self.token_emb = nn.Embedding(config.vocab_size, config.d_model)
+
+        self.layers = nn.ModuleList([Block(config) for _ in range(config.num_layers)])
+
+        self.norm = RMSNorm(config.d_model)
+        self.vocab_proj = nn.Linear(config.d_model, config.vocab_size, bias=False)
+
+        if config.weight_tying:
+            self.token_emb.weight = self.vocab_proj.weight
+
+        cos, isin = precompute_freqs_cis(config.d_model // config.num_heads, config.seq_len * 2)
+        self.register_buffer("freq_cos", cos)
+        self.register_buffer("freq_sin", isin)
+
+        if not hasattr(torch.nn.functional, "scaled_dot_product_attention"):
+            print("WARNING: using slow attention | upgrade pytorch to 2.0 or above")
+            mask = torch.full((1, 1, config.seq_len, config.seq_len), float("-inf"))
+            mask = torch.triu(mask, diagonal=1)
+            self.register_buffer("mask", mask)
+        else:
+            self.mask = None
+
+        self.apply(self._init_weights)
+
+    def forward(self, x: torch.Tensor):
+        batch, seqlen = x.shape
+        x = self.token_emb(x)
+        device = self.token_emb.weight.device
+        freqs_cis = self.freq_cos[:seqlen], self.freq_sin[:seqlen]
+
+        for layer in self.layers:
+            x = layer(x, self.mask, freqs_cis)
+
+        x = self.norm(x)
+        x = self.vocab_proj(x)
+        return x
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+
+class ModelingLM(nn.Module, PyTorchModelHubMixin):
+    def __init__(self, config: Config, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.config = config
+        self.model = Transformer(self.config)
+
+    def forward(self, x: torch.Tensor):
+        return self.model(x)
+
+
+if __name__ == "__main__":
+    config = Config(
+        vocab_size=10,
+        seq_len=10,
+        d_model=128,
+        hidden_dim=128,
+        num_heads=4,
+        num_kv_heads=0,
+        num_layers=4,
+        dropout=0.2,
+        bias=False,
+        weight_tying=False,
+        activation="silu",
+        mlp="GLU",
+    )
+
+    model = ModelingLM(config).eval()
+    print(model)
+    hf_name = "joey00072/test_001"
+    model.push_to_hub(hf_name)
+    x = torch.arange(10).reshape(1, 10)
+    print(model(x).sum(dim=-1))
+    new_model = ModelingLM.from_pretrained(hf_name).eval()
+    print(new_model(x).sum(dim=-1))
+
+    # assert torch.isclose(model.model.vocab_proj.weight, new_model.model.vocab_proj.weight)
---
+++ experiments/relu_next/train_relu_next.py
@@ -0,0 +1,306 @@
+import sys
+import time
+import math
+from datetime import datetime
+
+from typing import Any, Dict
+from dataclasses import dataclass, field
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+from ohara.lr_scheduler import CosineScheduler
+from ohara.dataset import PreTokenizedDataset
+from ohara.utils import BetterCycle
+
+from ohara.utils import auto_accelerator, random_name, model_summary
+
+
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+import lightning as L
+from lightning.pytorch.loggers import WandbLogger
+from lightning.fabric.loggers import TensorBoardLogger
+
+from model_relu_next import ModelingLM, Config
+from rich import print, traceback
+
+
+traceback.install()
+
+torch.set_float32_matmul_precision("high")
+
+### CONFIGS
+wandb_project_name = "Ohara-ReLU-Next"
+wandb_run_name = random_name()
+
+learning_rate: float = 5e-4
+min_learning_rate: float = 0.0
+
+warmup_iters: int = 1000
+max_iters: int = 10_000
+batch_size: int = 16
+micro_batch: int = 4
+eval_iters: int = 100
+save_ckpt_iters: int = 1000
+
+multiple_of: int = 4
+d_model: int = 1024 * 2 // 4
+hidden_dim = int(d_model * multiple_of)
+seq_len: int = 256
+num_layers: int = 32  # 44
+num_heads: int = 32
+
+
+assert d_model % num_heads == 0
+
+compile_model = not bool(sys.platform == "darwin")
+
+hf_name = "joey00072"
+ACTIVATION = "relu"
+MLP = "MLP"
+
+### Dataset and Tokenizer
+dataset_name = "joey00072/pretokenized__JeanKaddour_minipile__EleutherAI__gpt-neo-125m"  # run pretokeinze first
+tokenizer_name = "EleutherAI/gpt-neo-125m"
+
+### Setup
+device = auto_accelerator()  # auto chose device (cuda, mps)
+
+# for restarting training from last checkout
+resume_traning = False
+
+
+@torch.no_grad()
+def validate(
+    fabric: L.Fabric,
+    model: nn.Module,
+    dataloader: DataLoader,
+    max_iters: int,
+    ignore_index: int = -1,
+    device=None,
+) -> int:
+    fabric.barrier()
+    model.eval()
+    with torch.no_grad():
+        losses = torch.zeros(max_iters, device=device)
+        for idx, (data, target) in enumerate(dataloader):
+            if idx >= max_iters:
+                break
+            logits: torch.Tensor = model(data)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)),
+                target.reshape(-1),
+                ignore_index=ignore_index,
+            )
+            losses[idx] = loss.item()
+        val_loss = losses.mean()
+
+    model.train()
+    fabric.barrier()
+    return val_loss
+
+
+def train(
+    fabric: L.Fabric,
+    model: nn.Module,
+    optimizer: optim.Optimizer,
+    micro_batch: int,
+    train_dataloader,
+    val_dataloader,
+    eval_iters: int,
+    save_ckpt_iters: int,
+    get_lr,
+    ignore_index=-1,
+):
+    fabric.launch()
+    ignore_index = ignore_index if ignore_index else -1
+    # sanity test
+    validate(fabric, model, val_dataloader, 5, device=device)
+    # cyclining loder so you can runit indefinitely
+    train_dataloader = BetterCycle(iter(train_dataloader))
+    val_dataloader = BetterCycle(iter(val_dataloader))
+    (data, target) = next(val_dataloader)
+    tokerns_per_iter = int(math.prod(data.shape) * micro_batch)
+
+    micro_batch_loss: float = 0
+    idx: int = 0
+    while True:
+        micro_batch_loss = 0
+        if idx >= max_iters:
+            break
+        idx += 1
+        start_time: float = time.perf_counter()
+
+        lr = get_lr(idx)
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
+        # ...
+        for _ in range(micro_batch):
+            (data, target) = next(train_dataloader)
+            with fabric.no_backward_sync(model, enabled=micro_batch == 1):
+                logits: torch.Tensor = model(data)
+                loss = F.cross_entropy(
+                    logits.view(-1, logits.size(-1)),
+                    target.view(-1),
+                    ignore_index=ignore_index,
+                )
+                loss = loss / micro_batch
+                micro_batch_loss += loss.item()
+                fabric.backward(loss)
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+        curr_time: float = time.perf_counter()
+        elapsed_time: float = curr_time - start_time
+        print(
+            f"iter: {idx} | loss: {micro_batch_loss:.4f} | lr: {lr:e} | time: {elapsed_time:.4f}s"
+        )
+
+        log_dict = {
+                "traning_loss": micro_batch_loss,
+                # "test_loss": val_loss,
+                "iter": idx,
+                "tokens": idx * tokerns_per_iter,
+                "lr": lr,
+                "time": elapsed_time,
+            }
+
+        if idx % eval_iters == 0:
+            val_loss = validate(fabric, model, val_dataloader, 5, device=device)
+            log_dict["test_loss"] = val_loss
+            
+        fabric.log_dict(
+            log_dict,
+            step=idx,
+        )
+
+        if idx % save_ckpt_iters == 0:
+            state = {"model": model, "optimizer": optimizer, "idx": idx, "lr": lr}
+            fabric.save("./ckpt/model.pt", state)
+            model.push_to_hub(f"{hf_name}/relu_next_{ACTIVATION}")
+
+
+def main():
+    hyper_params = {
+        "learning_rate": learning_rate,
+        "min_learning_rate": min_learning_rate,
+        "warmup_iters": warmup_iters,
+        "max_iters": max_iters,
+        "eval_iters": eval_iters,
+        "batch_size": batch_size,
+        "micro_batch": micro_batch,
+        "d_model": d_model,
+        "seq_len": seq_len,
+        "num_layers": num_layers,
+        "num_heads": num_layers,
+        "multiple_of": multiple_of,
+        "compile_model": compile_model,
+        "tokenizer_name": tokenizer_name,
+        "dataset_name": dataset_name,
+        "resume_traning": resume_traning,
+        "save_ckpt_iters": save_ckpt_iters,
+        "activation": ACTIVATION,
+        "ffn": MLP,
+    }
+    # fabric init
+    # logger = WandbLogger(project=wandb_project_name, resume=resume_traning)
+    logger = TensorBoardLogger(root_dir="./logs", name=wandb_project_name)
+    fabric = L.Fabric(loggers=[logger], precision="bf16-mixed")
+    fabric.logger.log_hyperparams(hyper_params)
+
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+
+    config = Config(
+        vocab_size=tokenizer.vocab_size,
+        d_model=d_model,
+        seq_len=seq_len,
+        hidden_dim=hidden_dim,
+        num_kv_heads=0,
+        num_layers=num_layers,
+        num_heads=num_heads,
+        activation=ACTIVATION,
+        mlp=MLP,
+    )
+
+    train_ds = PreTokenizedDataset(
+        dataset_name=dataset_name,
+        tokenizer=tokenizer,
+        split="train",
+        hf=True,
+        max_length=seq_len,
+    )
+    test_ds = PreTokenizedDataset(
+        dataset_name=dataset_name,
+        tokenizer=tokenizer,
+        split="validation",
+        hf=True,
+        max_length=seq_len,
+    )
+
+    train_dataloader = DataLoader(train_ds, batch_size=batch_size)
+    test_dataloader = DataLoader(test_ds, batch_size=batch_size)
+    train_dataloader, test_dataloader = fabric.setup_dataloaders(train_dataloader, test_dataloader)
+
+    model = ModelingLM(config)
+    model: L.LightningModule = fabric.setup(model)
+    print("=" * 100)
+    if compile_model:
+        model = torch.compile(model)
+    # model.gradient_checkpointing_enable()
+    print(model)
+    print(model_summary(model))
+    import time
+
+    # exit()
+    get_lr = CosineScheduler(
+        learning_rate=learning_rate,
+        min_lr=min_learning_rate,
+        warmup_iters=warmup_iters,
+        max_iters=max_iters,
+    )
+
+    # inputs = torch.tensor(tokenizer.encode("The")).unsqueeze(0).clone().detach()
+    optimzer = optim.AdamW(model.parameters(), lr=get_lr(0))
+    optimzer = fabric.setup_optimizers(optimzer)
+    # Lets GO!!
+    train(
+        fabric,
+        model,
+        optimzer,
+        micro_batch=micro_batch,
+        train_dataloader=train_dataloader,
+        val_dataloader=test_dataloader,
+        eval_iters=eval_iters,
+        save_ckpt_iters=save_ckpt_iters,
+        get_lr=get_lr,
+        ignore_index=tokenizer.pad_token_id,
+    )
+
+    # TODO: Replace this inference
+    start: float = time.time()
+    max_tokens = 200
+    model = model.eval()
+    inputs = tokenizer.encode("Once")
+    with torch.no_grad():
+        inputs = torch.tensor(inputs).reshape(1, -1).to(device).clone().detach()
+        for _idx in range(max_tokens):
+            logits = model(inputs)
+            max_logits = torch.argmax(logits, dim=-1)
+            inputs: torch.Tensor = torch.cat((inputs, max_logits[:, -1:]), dim=-1)
+            inputs.shape[1] - 1
+            print(tokenizer.decode(inputs.tolist()[0][-1]), end="", flush=True)
+
+    end: float = time.time()
+
+    print(f"Time: {end-start}")
+
+
+if __name__ == "__main__":
+    main()
