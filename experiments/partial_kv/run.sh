f0b59fcf8899955f893cc80d3c7cbea1b888eb3f
joey00072 <00shxf@gmail.com>
2024-11-25 11:52:51 +0530

    :)

--- experiments/partial_kv/run.sh
+++ experiments/partial_kv/run.sh
@@ -3,10 +3,10 @@
 echo -e "Running partial KV experiment"
 
 echo -e "---- Baseline ----"
-python train.py attention_type=attention head_dim=32 num_heads=16
+python train.py attention_type=attention head_dim=64 num_heads=16
 
 echo -e "---- K == V ----"
-python train.py attention_type=k_is_v head_dim=32 num_heads=21
+python train.py attention_type=k_is_v head_dim=64 num_heads=21
 
 echo -e "---- Partial KV ----"
-python train.py attention_type=partial_kv head_dim=32 num_heads=18
+python train.py attention_type=partial_kv head_dim=64 num_heads=18
